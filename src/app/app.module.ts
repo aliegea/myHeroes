5c172d28e7d8752077016e8c98741a6ec62ceb51
Aliciaegea <aliegea4@gmail.com>
2022-03-23 16:15:50 +0100

    estructura inicial archivos y módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroesListComponent } from './pages/heroes/heroes-list/heroes-list.component';
+import { HeroDetailComponent } from './pages/heroes/hero-detail/hero-detail.component';
+import { TeamListComponent } from './pages/myTeam/team-list/team-list.component';
+import { HeroCardComponent } from './components/hero-card/hero-card.component';
+import { ConfirmAddComponent } from './components/confirm-add/confirm-add.component';
+import { ConfirmDeleteComponent } from './components/confirm-delete/confirm-delete.component';
+import { TeamFormComponent } from './pages/myTeam/team-form/team-form.component';
+import { ConfirmUpdateComponent } from './components/confirm-update/confirm-update.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeroesListComponent, HeroDetailComponent, TeamListComponent, HeroCardComponent, ConfirmAddComponent, ConfirmDeleteComponent, TeamFormComponent, ConfirmUpdateComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/confirm-add/confirm-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-add',
+  templateUrl: './confirm-add.component.html',
+  styleUrls: ['./confirm-add.component.css']
+})
+export class ConfirmAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
