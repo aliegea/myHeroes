8ffe497aef67a346ec4c4fd00a305d930e3acc6c
Aliciaegea <aliegea4@gmail.com>
2022-03-25 12:35:21 +0100

    set local storage y validaciones añadir héroe

--- src/app/pages/myTeam/team-list/team-list.component.ts
+++ src/app/pages/myTeam/team-list/team-list.component.ts
@@ -16,6 +16,7 @@ export class TeamListComponent implements OnInit {
   public teamHeroes: MySelectedHero[] = [];
   heroes!: Heroe[];
   heroe!: Heroe;
+  userData: any;
 
   constructor(
     private heroesServ: HeroesService,
@@ -27,32 +28,43 @@ export class TeamListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // this.teamServ.getSessionHero(this.teamHeroes);
+
     this.heroesServ.getHeroe().subscribe((resp) => {
       this.addToTeam(resp);
     });
-    this.teamHeroes = this.team;
+    this.teamHeroes = JSON.parse(
+      this.teamServ.getSessionHero(this.teamHeroes) || ''
+    );
+    this.teamServ.setData(this.teamHeroes);
   }
-  addToTeam(heroe: MySelectedHero) {
-    let newHeroe: MySelectedHero = new MySelectedHero();
-    newHeroe.id = heroe.id;
-    newHeroe.name = heroe.name;
-    newHeroe.thumbnail = heroe.thumbnail;
-    console.log(newHeroe);
-    this.teamHeroes.push(newHeroe);
-    console.log(this.teamHeroes);
 
-    this.teamServ.saveSessionStorage(this.teamHeroes);
+  addToTeam(heroe: MySelectedHero) {
+    let heroExists = false;
+    for (let i in this.teamHeroes) {
+      if (this.teamHeroes[i].id === heroe.id) {
+        heroExists = true;
+        break;
+      }
+    }
+
+    if (!heroExists && this.teamHeroes.length !== 6) {
+      let newHeroe: MySelectedHero = new MySelectedHero();
+      newHeroe.id = heroe.id;
+      newHeroe.name = heroe.name;
+      newHeroe.thumbnail = heroe.thumbnail;
+
+      this.teamHeroes.push(newHeroe);
+      this.teamServ.saveSessionStorage(this.teamHeroes);
+    }
   }
-  get team() {
-    return JSON.parse(this.teamServ.getSessionHero(this.teamHeroes) || '');
-  }
+
   removeHeroe(heroeToRemove: MySelectedHero) {
     for (let i = 0; i < this.teamHeroes.length; i++) {
       if (this.teamHeroes[i] == heroeToRemove) {
         this.teamHeroes.splice(i, 1);
-        console.log(heroeToRemove);
       }
     }
-    this.teamServ.saveSessionStorage(this.teamHeroes);
+    this.teamServ.setData(this.teamHeroes);
   }
 }
--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
+import { MySelectedHero } from '../interfaces/MySelectedHero';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TeamService {
-  sessionHeroeName: string = 'MyHeroes';
+  sessionHeroeName: string = 'userData';
   constructor() {}
 
   setData(data: any) {
     const jsonData = JSON.stringify(data);
     localStorage.setItem('myData', jsonData);
   }
-
-  getSessionHero(team: any[]) {
+  getSessionHero(data: any) {
     if (sessionStorage.getItem(this.sessionHeroeName) == null) {
-      this.saveSessionStorage(team);
+      this.saveSessionStorage(data);
     }
     return sessionStorage.getItem(this.sessionHeroeName);
   }
 
-  saveSessionStorage(team: any) {
-    sessionStorage.setItem(this.sessionHeroeName, JSON.stringify(team));
+  saveSessionStorage(data: any[]) {
+    sessionStorage.setItem(this.sessionHeroeName, JSON.stringify(data));
   }
 }
